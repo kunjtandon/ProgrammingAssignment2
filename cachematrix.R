9f59101543188f28b3a7ff01de96664550c06c28
kunjtandon <Kunj.Tandon@BNGSTI-L-97729.asia-pac.shell.com>
2015-11-21 02:32:24 +0530

    Assignment 2
    Kunj Tandon

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+ 
+        setMatrix <- function(matrix = matrix()){
+                x <<- matrix
+        }
+ 
+        getMatrix <- function() x
+ 
+        setInverse <- function(inverseMatrix = matrix()){ 
+                inverse <<- inverseMatrix
+        } 
 
+        getInverse <- function() inverse
+        ## The list below is returned by the function. cacheSolve needs this list as an argument.
+        list(get = getMatrix, set = setMatrix, getInverse = getInverse, setInverse = setInverse)
 }
 
 
@@ -12,4 +26,38 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of x
+        ## Matrix assumed invertible.  
+ 
+        if(!is.null(x$getInverse())){
+                print("Matrix was already in the cache; do nothing.")
+        }
+        else {
+	         print("Matrix was not cached hence inverse computed.") 
+               x$setI(solve(x$get()))
+	  }
+ 
+        x$getInverse()
 }
+
+##Sample Run
+## x <- matrix(1:9,3,3)
+## x[2,2] <- 100     "This makes the matrix invertible"
+## y <- makeCacheMatrix(x)
+## z <- cacheSolve(y)
+## [1] "Matrix was not cached hence inverse computed."
+## > z
+##             [,1]         [,2]         [,3]
+## [1,] -0.747368421 -0.005263158  0.585964912
+## [2,] -0.005263158  0.010526316 -0.005263158
+## [3,]  0.252631579 -0.005263158 -0.080701754
+##
+## z <- cacheSolve(y)
+## [1] "Matrix was already in the cache; do nothing."
+##
+##
+
+
+
+##
+
